a2b509e5623b895522218ec2f1dc1abd1ed3a76b
Svetlan-ka <sv.chapova@yandex.ru>
2024-10-04 12:43:43 +0700

    task 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,31 @@
 package org.example;
 
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        List<Thread> threads = new ArrayList<>();
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        List<Integer> futureResults = new ArrayList<>();
+
         String[] texts = new String[25];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("aab", 30_000);
         }
 
         long startTs = System.currentTimeMillis(); // start time
-        for (String text : texts) {
-            Thread thread = new Thread(() -> {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
-                            }
-                        }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
-                    }
-                }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-            });
-            threads.add(thread);
-            thread.start();
-        }
-        for (Thread thread : threads) {
-            thread.join();
+        for (int i = 0; i < texts.length; i++) {
+            MyCallable task = new MyCallable(texts[i]);
+            Future<Integer> futureMaxSize = executorService.submit(task);
+            futureResults.add(futureMaxSize.get());
         }
         long endTs = System.currentTimeMillis(); // end time
         System.out.println("Time: " + (endTs - startTs) + "ms");
+        executorService.shutdown();
+
+        int maxInterval = Collections.max(futureResults);
+        System.out.println("Максимальный интервал: " + maxInterval);
     }
 
     public static String generateText(String letters, int length) {
@@ -51,5 +35,39 @@ public class Main {
             text.append(letters.charAt(random.nextInt(letters.length())));
         }
         return text.toString();
+    }
+}
+
+class MyCallable implements Callable<Integer> {
+    String text;
+    Integer maxSize;
+
+    public MyCallable(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+
+        int maxSize = 0;
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < text.length(); j++) {
+                if (i >= j) {
+                    continue;
+                }
+                boolean bFound = false;
+                for (int k = i; k < j; k++) {
+                    if (text.charAt(k) == 'b') {
+                        bFound = true;
+                        break;
+                    }
+                }
+                if (!bFound && maxSize < j - i) {
+                    maxSize = j - i;
+                }
+            }
+        }
+        System.out.println(text.substring(0, 100) + " -> " + maxSize);
+        return maxSize;
     }
 }
